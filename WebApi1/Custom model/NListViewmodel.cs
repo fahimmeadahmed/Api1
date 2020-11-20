7c9a9fd4dc82384586eedb8e00ba091ac4166213
fahimmeadahmed <fahimmeadahmed@gmail.com>
2020-11-20 10:42:51 +0600

    webAPi

---
+++ WebApi1/Custom model/NListViewmodel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApi1.Custom_model
+{
+    public class NListViewmodel
+    {
+       
+        public int id { get; set; }
+       
+        public string title { get; set; }
+
+       
+        public string description { get; set; }
+
+
+        public DateTime date { get; set; }
+    }
+}
+
